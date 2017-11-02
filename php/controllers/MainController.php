73a2735d61c82b855a881eee1391dc7179de8598
givi <arabidzegivi@gmail.com>
2017-11-02 10:30:48 -0700

    gender added 1 => dedali, 2 => mamali

--- php/controllers/MainController.php
+++ php/controllers/MainController.php
@@ -12,6 +12,7 @@ class MainController {
 	function getList() {
             $params = isset($_REQUEST['params']) ? $_REQUEST['params'] : '';
             $position = isset($_REQUEST['position']) ? $_REQUEST['position'] : 1;
+            $gender = isset($_REQUEST['gender']) ? $_REQUEST['gender'] : 1;
             $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : -1;
             
             $response = array();
@@ -19,24 +20,33 @@ class MainController {
             
             switch($params) {
                 case 'main_parts':
-                    $response = $model->getBodyParts($position);
+                    $response = array('front' => array(),
+                        'back' => array(),
+                        'parts' => array(),
+                        'skin' => array('ID' => 11, 'name' => 'კანი', 'position' => 0, 'sub_parts' => array())
+                    );  
+                    $response['parts'] = $model->getBodyParts($position, $gender, 11);
                     break;
                 case 'sub_parts':
                     if($id < 0) {
                         $this->error['error'] .= ' ID not passed';
                         return $this->error;
                     }
-                    $response = $model->getSubBodyParts($id, $position);
+                    $response = $model->getSubBodyParts($id, $position, $gender, 11);
                     break;
                 case 'symptoms':
+                    //TO-DO: add functionality to get symptoms by age and gender (t_symptom_map.age_id,gender)
                     if($id < 0) {
                         $this->error['error'] .= ' ID not passed';
                         return $this->error;
                     }
                     $response = $model->getSymptoms($id);
                     break;
+                case 'age_list':
+                    $response = $model->getAgeList();
+                    break;
                 default:
-                    $response = $model->getStructure();
+                    $response = $model->getStructure($gender);
             }
             
             return $response;
--- php/models/MainModel.php
+++ php/models/MainModel.php
@@ -8,40 +8,51 @@ class MainModel {
 		$this->sql = new sql();
 	}
         
-        function getBodyParts($position, $skin=-1) {
+        function getBodyParts($position, $gender, $skin=-1) {
             $db = $this->sql;
             
-            $query = "select * from t_body_parts b where b.position in (0,:position) and id != ".$skin;
-            $params = array(0 => array('name' => ':position', 'value' => $position, 'type' => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 'size' => -1));
+            $query = "select * from t_body_parts b where b.position = :position and b.gender = :gender and id != ".$skin;
+            $params = array(0 => array('name' => ':position', 'value' => $position, 'type' => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 'size' => -1),
+                1 => array('name' => ':gender', 'value' => $gender, 'type' => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 'size' => -1));
             
-            return $db->execStatement($query, $params);      
+            return $db->execStatement($query, $params); 
         }
         
-        function getSubBodyParts($id, $position) { 
+        function getAgeList() {
+            $db = $this->sql;
+            
+            $query = "select id, display_name from t_age g";
+            
+            return $db->execStatement($query);      
+        }
+        
+        function getSubBodyParts($id, $position, $gender) { 
             $db = $this->sql;
             
             $query = "SELECT s.id, s.name FROM t_body_map m, t_sub_body_parts s
                         where m.sub_body_parts_id = s.id
                           and m.body_parts_id = :id
-                          and m.location in (0,:position)";
+                          and s.position = :position
+                          and s.gender = :gender";
             $params = array(0 => array('name' => ':id', 'value' => $id, 'type' => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 'size' => -1),
-                    1 => array('name' => ':position', 'value' => $position, 'type' => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 'size' => -1));
+                    1 => array('name' => ':position', 'value' => $position, 'type' => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 'size' => -1),
+                    2 => array('name' => ':gender', 'value' => $gender, 'type' => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT, 'size' => -1));
             
             return $db->execStatement($query, $params);      
         }        
         
-        function getStructure() {
+        function getStructure($gender) {
             $body_parts = array('front' => array(),
                         'back' => array(),
                         'skin' => array('ID' => 11, 'name' => 'კანი', 'position' => 0, 'sub_parts' => array())
                 );   
-            $body_parts['front'] = $this->getBodyParts(1,11);
-            $body_parts['back'] = $this->getBodyParts(2,11);
+            $body_parts['front'] = $this->getBodyParts(1, $gender, 11);
+            $body_parts['back'] = $this->getBodyParts(2, $gender, 11);
             
             for($i=0; $i<sizeof($body_parts['front']); $i++)
-                $body_parts['front'][$i]['sub_parts'] = $this->getSubBodyParts ($body_parts['front'][$i]['ID'], 1);
+                $body_parts['front'][$i]['sub_parts'] = $this->getSubBodyParts ($body_parts['front'][$i]['ID'], 1, $gender);
             for($i=0; $i<sizeof($body_parts['back']); $i++)
-                $body_parts['back'][$i]['sub_parts'] = $this->getSubBodyParts ($body_parts['back'][$i]['ID'], 2);
+                $body_parts['back'][$i]['sub_parts'] = $this->getSubBodyParts ($body_parts['back'][$i]['ID'], 2, $gender);
             
             return $body_parts;
         }
