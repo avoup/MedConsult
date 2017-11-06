46fd3c8ebf9d512c15ba601db9e7c7f3424a6ffb
avoup <avoup.g@gmail.com>
2017-11-06 19:52:50 +0400

    loader.php fix; + request;

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -25,80 +25,110 @@ $(document).ready(function(){
      document.getElementById("mySidenav").style.width = "0";
   });
 
-//svg-positioning
-      a.addEventListener("load",function(){
-          var svgDoc = a.contentDocument;
-          svg = svgDoc.getElementById("figure");
+  a.addEventListener("load",function(){
+      var svgDoc = a.contentDocument;
+      svg = svgDoc.getElementById("figure");
+      var svgi = svgDoc.getElementById("outer");
+      var showThis, hideThis;
+
+        // $.ajax ({
+        //   url: 'php/loader.php?params=main_parts',
+        //   type: "post",
+        //   // data: $('#username').serialize(),
+        //   // datatype: "JSON",
+        //   beforeSend: function(){
+        //     console.log("loading");
+        //   },
+        //   success: function(response){
+        //
+        //     var resp = $.parseJSON(response);
+        //     $.each(resp.parts, function(i,v){
+        //
+        //       console.log(v);
+        //       $(svgi).append("<path " + v.image + " />")
+        //
+        //
+        //     })
+        //     // $("#errorplace").html(answer.response);
+        //
+        //   },
+        //   error: function(error){
+        //
+        //     var resp = $.parseJSON(error);
+        //
+        //     console.log(resp.response);
+        //   }
+        // })
 
 
+      $(svg).click(function(e){
 
-          var showThis, hideThis;
-          $(svg).click(function(e){
+        var scale = 4;
+        var name = e.target.dataset.name;
+        $("#zoom-out").show(500);
 
-            var scale = 4;
-            $("#zoom-out").show(500);
+        $(showThis).css("display","none");
+        $(hideThis).css("display","block");
 
-            $(showThis).css("display","none");
-            $(hideThis).css("display","block");
-
-            switch(e.target.dataset.name){
-              case "head":
-                position = [0,280];
-                break;
-              case "neck":
-                position = [0,220];
-                break;
-              case "chest":
-                position = [0,190];
-                break;
-              case "back":
-                position = [0,140]
-                scale = 3.5;
-                break;
-              case "abdomen":
-                position = [0,100];
-                break;
-              case "pelvis":
-              case "buttock":
-                position = [0,10];
-                scale = 3.7;
-                break;
-              case "hand":
-                position = [100,80];
-                scale = 2.2;
-                break;
-              case "leg":
-                position = [0,-150];
-                scale = 1.8;
-                break;
-              default:
-                position = [0,0];
-                scale = 1;
-                $("#zoom-out").hide();
-            }
-
-            showThis = svgDoc.getElementById(sex[0] + "-" + view[0] + "-i-" + e.target.dataset.name);
-            hideThis = svgDoc.getElementById(sex[0] + "-" + view[0] + "-" + e.target.dataset.name);
-            $(showThis).css("display","block");
-            $(hideThis).css("display","none");
-
-            $(".svg-path").css("transform", "scale(" + scale + ") translate(" + position[0] + "px," + position[1] + "px)")
-          });
-
-          $("#zoom-out").click(function(){
+        switch(name){
+          case "head":
+            position = [0,280];
+            break;
+          case "neck":
+            position = [0,220];
+            break;
+          case "chest":
+            position = [0,190];
+            break;
+          case "back":
+            position = [0,140]
+            scale = 3.5;
+            break;
+          case "abdomen":
+            position = [0,100];
+            break;
+          case "pelvis":
+          case "buttock":
+            position = [0,10];
+            scale = 3.7;
+            break;
+          case "hand":
+            position = [100,80];
+            scale = 2.2;
+            break;
+          case "leg":
+            position = [0,-150];
+            scale = 1.8;
+            break;
+          default:
             position = [0,0];
             scale = 1;
-            $(showThis).css("display","none");
-            $(hideThis).css("display","block");
-            $(this).hide(500);
-            $(".svg-path").css("transform", "scale(" + scale + ") translate(" + position[0] + "px," + position[1] + "px)")
+            $("#zoom-out").hide();
+        }
 
-          });
-      }, false);
+        showThis = svgDoc.getElementById(sex[0] + "-" + view[0] + "-i-" + name);
+        hideThis = svgDoc.getElementById(sex[0] + "-" + view[0] + "-" + name);
+        $(showThis).css("display","block");
+        $(hideThis).css("display","none");
 
-    function posit(){
-      svg.setAttribute("viewBox", pos + " 0 400 700");
-    };
+        $(".svg-path").css("transform", "scale(" + scale + ") translate(" + position[0] + "px," + position[1] + "px)")
+      });
+
+      $("#zoom-out").click(function(){
+        position = [0,0];
+        scale = 1;
+        $(showThis).css("display","none");
+        $(hideThis).css("display","block");
+        $(this).hide(500);
+        $(".svg-path").css("transform", "scale(" + scale + ") translate(" + position[0] + "px," + position[1] + "px)")
+
+      });
+    }, false);
+//svg-positioning
+
+  function posit(){
+    svg.setAttribute("viewBox", pos + " 0 400 700");
+  };
 
 
 
