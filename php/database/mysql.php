3af2a91ad95555fa697333c074e54d4ade9a5aa0
givi <arabidzegivi@gmail.com>
2017-12-08 06:03:51 -0800

    fuck dis shit :@

--- php/database/mysql.php
+++ php/database/mysql.php
@@ -1,8 +1,8 @@
 <?php
-define('server', 'localhost');
+define('server', '82.196.3.33');
 define('testdb', 'symptom_checker');
 define('companydb', 'symptom_checker');
-define('dbuser', 'root');
-define ('password', '');
+define('dbuser', 'admin');
+define ('password', '78c6d1f755082c76a685fbbf18f15c9105aebb381fef0531');
 define('connection_string',  'mysql:host='.server.';dbname='.companydb.';charset=utf8');
 ?>
